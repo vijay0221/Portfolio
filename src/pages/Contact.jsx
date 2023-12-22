7cca34618ee80dfb7e215bd1fa0b36ba3d368154
vijay0221 <vijaymaroju2003@gmail.com>
2023-12-22 18:54:00 +0530

    sd

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,13 +5,15 @@ import useAlert from "../hooks/useAlert";
 import Alert from "../components/Alert";
 import { socialLinks } from "../constants";
 import { Link } from "react-router-dom";
+import image from '../assets/icons/twitter.svg'
+import github from '../assets/icons/github.svg'
+import  linkedin  from "../assets/icons/linkedin.svg";
+
 
 function Contact() {
   const [form ,setForm]=useState({name:"",email:"",message:''})
   const [isLoading, setIsLoading]=useState(false)
-  const fromRef=useRef(null);
-  
-  const [currentAnimation,setCurrentAnimation]=useState('idle')
+
   const  {alert,showAlert,hideAlert}=useAlert();
 
   const handleChange=(e)=>{
@@ -19,22 +21,23 @@ function Contact() {
   };
 
   const handleSubmit=(e)=>{
+    console.log("entered here")
+    
     e.preventDefault();
     setIsLoading(true);
     setCurrentAnimation('hit')
     emailjs.send(
-     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-    {
-      from_name:form.name,
-      to_name:"Vijay",
-      from_email:form.email,
-      to_email:'vijaykumar022103@gmail.com',
-      message:form.message
-    },
-    import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-   
-    ).then(()=>{
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+          from_name: form.name,
+          to_name: "Vijay",
+          from_email: form.email, // Use the email from the form
+          to_email: 'vijaykumar022103@gmail.com', // Specify the desired recipient email
+          message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+  ).then(()=>{
       setIsLoading(false);
       showAlert({show:true,text:"message sent succefulluly" , type:'success'})
       setTimeout(()=>
@@ -42,10 +45,9 @@ function Contact() {
         setCurrentAnimation('idle')
       setForm({name:'',email:"",message:""})
         
-      },[3000])
+      },[2000])
     }).catch((error)=>{
       setIsLoading(false)
-      setCurrentAnimation('idle')
       console.log(error)
        showAlert({show:true,text:"I didn't recieve your message", type:'danger'})
     })
@@ -56,10 +58,10 @@ function Contact() {
   return (
    <section className=" relative flex lg:flex-row flex-col max-container h-screen">
     {alert.show&& <Alert {...alert}/>}
-    
+     
     <div className=" flex-1 min-w-[50%] flex flex-col">
-         <h1 className="head-text">Get In Touch</h1>
-         <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
+         <h1 className="head-text">Get In Touch</h1>         
+         <form  className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
             <label className="text-black-500 font-semibold">
               Name
              <input 
@@ -105,10 +107,28 @@ function Contact() {
             {isLoading? 'Sending...' :'Send Message'}
             </button>
          </form>
+        
+        
+        <div className="flex flex-col gap-4 py-4 mt-5">
+         <p className=" text-lg font-semibold py-2 mt-2">Reach me out and see my works from below links:</p>
+         <div className="flex items-center gap-11">
+         <Link to='https://twitter.com/Vijay200321' className="flex items-center flex-col">
+             <img src={image} width={38}/>
+             <p className="text-sm font-semibold  text-black-500">Twitter</p>
+         </Link>
+         <Link to='https://github.com/vijay0221' className="flex items-center flex-col">
+             <img src={github} width={38}/>
+             <p className="text-sm font-semibold text-black-500">Git Hub</p>
+         </Link>
+         <Link to='https://www.linkedin.com/in/vijay-kumar-78329727b/' className="flex items-center flex-col">
+              <img src={linkedin} width={38}/>
+              <p className="text-sm font-semibold text-black-500">linkedin</p>
+         </Link>
+         </div>
+        </div>
      
     </div>
 
-   
   
    </section>
   )
